152ab362fe1b46ffc4fdf12f5fe162b047ed406d
uday <udkumar@hotmail.com>
2019-04-02 02:45:32 +0530

    Intro in Go

---
+++ gointro.go
@@ -0,0 +1,12 @@
+package main // go program consist of packages, and start with main
+
+//import a library/package, here we import fmt, which bring standard formatted I/O  string from C
+import (
+	"fmt"
+)
+
+// Syntax for a function in Go, 
+func main(){
+	// Println is a function from the fmt library
+	fmt.Println("Welcome to GoLang!")
+}
