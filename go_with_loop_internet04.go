426de0769f406cfc60a291871257caf328d6642e
uday <udkumar@hotmail.com>
2019-04-08 00:47:26 +0530

    xml parshing with loop

---
+++ go_with_loop_internet04.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Sitemapindex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
+func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	var s Sitemapindex
+	xml.Unmarshal(bytes, &s)
+	for _, Location := range s.Locations {
+		fmt.Printf("%s\n", Location)
+	}
+	//fmt.Println(s.Locations)
+}
