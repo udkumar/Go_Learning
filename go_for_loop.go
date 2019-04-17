7f6d1e8466c72be2c13cf3a4dd5b54c2128024af
uday <udkumar@hotmail.com>
2019-04-17 17:05:21 +0530

    added continue in loop with if condition

--- go_for_loop.go
+++ go_for_loop.go
@@ -18,6 +18,13 @@ func main() {
 		fmt.Println("loop")
 		break
 	}
+	//You can also continue to the next iteration of the loop.
+	for n := 0; n<=5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
 
 	
 }
