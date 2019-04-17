c48f21d9a43a81d647a70ddc2b90f83689b85a51
uday <udkumar@hotmail.com>
2019-04-17 16:56:06 +0530

    added loop for xml parsing

--- go_with_loop_internet04.go
+++ go_with_loop_internet04.go
@@ -15,17 +15,24 @@ type Location struct {
 	Loc string `xml:"loc"`
 }
 
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
 func (l Location) String() string {
 	return fmt.Sprintf(l.Loc)
 }
 
 func main() {
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	var s Sitemapindex
-	xml.Unmarshal(bytes, &s)
-	for _, Location := range s.Locations {
-		fmt.Printf("%s\n", Location)
-	}
+	 resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	 bytes, _ := ioutil.ReadAll(resp.Body)
+	 var s Sitemapindex
+	 xml.Unmarshal(bytes, &s)
+	 for _, Location := range s.Locations {
+	 	fmt.Printf("%s\n", Location)
+	 }
 	//fmt.Println(s.Locations)
+
 }
